abf1df241aab048deae729b127dc59fa45a3e721
Ashwini277 <devarushiashwini@gmail.com>
2024-08-30 09:31:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    const inputField = document.getElementById('fname');
+
+    // Add an event listener to the input field for the blur event
+    inputField.addEventListener('blur', function() {
+        // Convert the input value to uppercase
+        this.value = this.value.toUpperCase();
+    });
+});
+
